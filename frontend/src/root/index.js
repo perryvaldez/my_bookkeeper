c2fbfbdaa396dba98391d0aa87bcea62e7e80609
Perry Valdez <perryv@gmail.com>
2018-08-18 22:05:40 +0800

    Set up internationalization

--- frontend/src/root/index.js
+++ frontend/src/root/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { IntlProvider, intlReducer } from 'react-intl-redux'
+import { createStore, combineReducers } from 'redux';
 import { rootReducer } from '../store/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import App from '../app/App';
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const reducers = combineReducers({ ...rootReducer, intl: intlReducer });
+const store = createStore(reducers, devToolsEnhancer());
 
 const root = (
   <Provider store={store}>
-    <App />
+    <IntlProvider locale={navigator.language}>
+      <App />
+    </IntlProvider>
   </Provider>
 );
 
