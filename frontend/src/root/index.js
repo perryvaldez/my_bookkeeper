9f6439b22ea4f2a2d6fe649287072cde916ef902
Perry Valdez <perryv@gmail.com>
2018-08-18 22:19:01 +0800

    Installed react router

--- frontend/src/root/index.js
+++ frontend/src/root/index.js
@@ -4,6 +4,7 @@ import { IntlProvider, intlReducer } from 'react-intl-redux'
 import { createStore, combineReducers } from 'redux';
 import { rootReducer } from '../store/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../app/App';
 
 const reducers = combineReducers({ ...rootReducer, intl: intlReducer });
@@ -12,7 +13,9 @@ const store = createStore(reducers, devToolsEnhancer());
 const root = (
   <Provider store={store}>
     <IntlProvider locale={navigator.language}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </IntlProvider>
   </Provider>
 );
