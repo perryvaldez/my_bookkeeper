6d06b07623c938dd67a816697a6f0269451a2adb
Perry Valdez <perryv@gmail.com>
2018-08-20 14:01:12 +0800

    Added token cookie generation

--- backend/server/models/appuser.js
+++ backend/server/models/appuser.js
@@ -1,5 +1,9 @@
-'use strict';
-
 module.exports = function(AppUser) {
-
+  AppUser.afterRemote('login', async (ctx) => {
+    ctx.res.cookie('access_token', ctx.result.id, { signed: true, maxAge: ctx.result.ttl * 1000, httpOnly: true });
+  });
+    
+  AppUser.afterRemote('logout', async (ctx) => {
+    ctx.res.clearCookie('access_token');
+  });
 };
